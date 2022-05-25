a0098c6d16a446c988ae1a27868d273acb39ce40
AndreySo1 <andrey.kafka@gmail.com>
2022-05-25 15:20:18 +0300

    fif: fix task 3, check folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,10 +4,24 @@ const path = require('path');
 fs.readdir(path.join(__dirname, 'secret-folder'), 'utf8', (err, data) => {
    if (err) throw err;
  
-   console.log(data);
+   //console.log(data);
    data.forEach(element => {
-      let elemOb = path.parse(element);
-      console.log(`${elemOb.name} - ${elemOb.ext} - `);
+      
+      let elem =element;
+      let elemPath= path.join(__dirname, 'secret-folder', elem);
+ 
+      fs.stat(elemPath, 'utf-8',  (error, stats) => {
+         
+         let elemOb = path.parse(element);
+         let ext= elemOb.ext;
+         
+         if(stats.size == 0){
+            ext = 'Folder';
+         }
+
+         console.log(`${elemOb.name} - ${ext} - ${stats.size ? stats.size/1000+'kb' : 'Folder'} `);
+      });
+
    });
 
  });
